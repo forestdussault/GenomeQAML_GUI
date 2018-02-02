98603744db9c2c8a80e97e53f14e591f7fdf75b0
Forest Dussault <forest.dussault@inspection.gc.ca>
2018-02-02 16:09:46 -0500

    0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="genomeqaml-gui",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     author="Forest Dussault",
     author_email="forest.dussault@inspection.gc.ca",
